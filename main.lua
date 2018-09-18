b1d5fc1014916fff8a308ab9d2e512c76ca408d3
Jonas Thiem <jonthiem@gmail.com>
2018-09-18 02:48:45 +0200

    Initial commit

---
+++ main.lua
@@ -0,0 +1,75 @@
+-- Copyright 2018 Jonas Thiem
+
+-- Data
+local partyAddress = 0x0000DCD8
+
+-- code
+
+local prevParty = {0, 0, 0, 0, 0, 0}
+local party = {0, 0, 0, 0, 0, 0}
+
+local function copyArray(a, b)
+    for i, v in pairs(a) do
+        b[i] = v
+    end
+end
+
+local function readParty()
+    copyArray(party, prevParty)
+    for  i = 1, 6, 1 do
+        local p = memory.readbyteunsigned(partyAddress + i - 1)
+        if p ~= 255 then
+            party[i] = p
+        else
+            party[i] = 0
+        end
+    end
+end
+
+local function didPartyChange()
+    local r = {false, false, false, false, false, false, false}
+    
+    for i = 1, 6, 1 do
+        if prevParty[i] ~= party[i] then
+            r[1] = true
+            r[i + 1] = true 
+        end
+    end
+
+    return r
+end
+
+local function update()
+    readParty()
+
+    local changeMap = didPartyChange()
+    if changeMap[1] then
+        for i = 1, 6, 1 do
+            if changeMap[i + 1] then
+                local newPNG = io.open("./sprites/" .. tostring(party[i]) .. ".png", "rb") 
+                local newData = newPNG:read("*a")
+                newPNG:flush()
+                
+                local oldPNG = io.open("./party/p" .. tostring(i) .. ".png", "wb")
+                oldPNG:write(newData)
+                oldPNG:flush()
+            end
+        end
+    end
+end
+
+local function clearPartyDisplay()
+    local newPNG = io.open("./sprites/0.png", "rb+")
+    local newData = newPNG:read("*a")
+    newPNG:flush()
+
+    for i = 1, 6, 1 do
+        local oldPNG = io.open("./party/p" .. tostring(i) .. ".png", "wb+")
+        oldPNG:write(newData)
+        oldPNG:flush()
+    end
+end
+
+clearPartyDisplay()
+gui.register(update)
+vba.print("g2pd 1.0 loaded <3")
---
+++ readme.md
@@ -0,0 +1,19 @@
+# g2pd - Gen 2 Pokemon Display
+
+## What?
+This script allows streamers to display their current party to their viewers.
+
+## Setup
+1. Get [Viusal Boy Advanced Rerecording](https://github.com/TASVideos/vba-rerecording/releases) if you don't already use it.
+
+2. [Get the latest version of g1pd](https://github.com/PeachIceTea/g1pd/archive/master.zip).
+
+3. Get sprites you like, the only requirement being that they need to be named with their corresponding Pokédex number. I used [these](https://veekun.com/dex/downloads). Also, there needs to be a 0.png that matches the dimensions of the other sprites but is emtpy.
+
+4. Extract g1pd into its own folder.
+
+5. Extract the sprites into the "sprites" folder.
+
+6. Run VBA and when the game is loaded. Load the script from Tools/Lua Scripting/New Lua Script Window...
+
+7. Add the png files in the "party" folder to your OBS overlay.

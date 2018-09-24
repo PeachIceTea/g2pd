beee31dd42120fbeeb7f074a98557034fff64db0
Jonas Thiem <jonthiem@gmail.com>
2018-09-24 20:57:17 +0200

    Add ability to use named sprites

--- main.lua
+++ main.lua
@@ -2,7 +2,52 @@
 
 -- Data
 local partyAddress = 0x0000DCD8
+local numberedSprites
 
+local numberPNG = io.open("./sprites/1.png", "rb+")
+if numberPNG ~= nil then
+    vba.print("Using numbered sprites.")
+    numberedSprites = true
+    numberPNG:flush()
+else
+    vba.print("Using named sprites.")
+    numberedSprites = false
+end
+
+local pkmNameDb = {"Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard",
+    "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree",
+    "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate",
+    "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash",
+    "NidoranF", "Nidorina", "Nidoqueen", "NidoranM", "Nidorino", "Nidoking",
+    "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff",
+    "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth",
+    "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape",
+    "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam",
+    "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel",
+    "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro",
+    "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk",
+    "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno",
+    "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak",
+    "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey",
+    "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie",
+    "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados",
+    "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar",
+    "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres",
+    "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew",
+    
+    "Chikorita", "Bayleef", "Meganium", "Cyndaquil", "Quilava", "Typhlosion",
+    "Totodile", "Croconaw", "Feraligatr", "Sentret", "Furret", "Hoothoot", "Noctowl",
+    "Ledyba", "Ledian", "Spinarak", "Ariados", "Crobat", "Chinchou", "Lanturn", "Pichu", "Cleffa",
+    "Igglybuff", "Togepi", "Togetic", "Natu", "Xatu", "Mareep", "Flaaffy", "Ampharos", "Bellossom",
+    "Marill", "Azumarill", "Sudowoodo", "Politoed", "Hoppip", "Skiploom", "Jumpluff", "Aipom",
+    "Sunkern", "Sunflora", "Yanma", "Wooper", "Quagsire", "Espeon", "Umbreon", "Murkrow", "Slowking",
+    "Misdreavus", "Unown", "Wobbuffet", "Girafarig", "Pineco", "Forretress", "Dunsparce", "Gligar",
+    "Steelix", "Snubbull", "Granbull", "Qwilfish", "Scizor", "Shuckle", "Heracross", "Sneasel",
+    "Teddiursa", "Ursaring", "Slugma", "Magcargo", "Swinub", "Piloswine", "Corsola", "Remoraid", "Octillery",
+    "Delibird", "Mantine", "Skarmory", "Houndour", "Houndoom", "Kingdra", "Phanpy", "Donphan",
+    "Porygon2", "Stantler", "Smeargle", "Tyrogue", "Hitmontop", "Smoochum", "Elekid", "Magby", "Miltank",
+    "Blissey", "Raikou", "Entei", "Suicune", "Larvitar", "Pupitar", "Tyranitar", "Lugia", "Ho-Oh", "Celebi"
+}
 -- code
 
 local prevParty = {0, 0, 0, 0, 0, 0}
@@ -41,6 +86,14 @@ local function didPartyChange()
     return r
 end
 
+local function getPNGPath(id)
+    if numberedSprites then
+        return "./sprites/" .. id .. ".png"
+    else
+        return "./sprites/" .. pkmNameDB[id] .. ".png"
+    end
+end
+
 local function update()
     readParty()
 
@@ -49,14 +102,23 @@ local function update()
         for i = 1, 6, 1 do
             if changeMap[i + 1] then
                 local id = party[i]
-                if id == 0xfd then id = "egg" end
-                local newPNG = io.open("./sprites/" .. id .. ".png", "rb") 
-                local newData = newPNG:read("*a")
-                newPNG:flush()
-                
-                local oldPNG = io.open("./party/p" .. tostring(i) .. ".png", "wb")
-                oldPNG:write(newData)
-                oldPNG:flush()
+                local pngPath
+                if id == 0xfd then
+                    pngPath = "./sprites/egg.png"
+                else
+                    pngPath = getPNGPath(id)
+                end
+                local newPNG = io.open(pngPath, "rb")
+                if newPNG == nil then
+                    vba.print(getPNGPath(party[i]) .. " is missing.")
+                else
+                    local newData = newPNG:read("*a")
+                    newPNG:flush()
+                    
+                    local oldPNG = io.open("./party/p" .. tostring(i) .. ".png", "wb")
+                    oldPNG:write(newData)
+                    oldPNG:flush()
+                end
             end
         end
     end
@@ -64,6 +126,7 @@ end
 
 local function clearPartyDisplay()
     local newPNG = io.open("./sprites/0.png", "rb+")
+    if newPNG == nil then vba.print("0.png is missing in the sprites folder.") end
     local newData = newPNG:read("*a")
     newPNG:flush()
 
@@ -76,4 +139,4 @@ end
 
 clearPartyDisplay()
 gui.register(update)
-vba.print("g2pd 1.1 loaded <3")
+vba.print("g2pd 1.2 loaded <3")
